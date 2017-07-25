c7b7f3c8440cdcdf15751c37cb85a32944cde0d2
koertho <t.koerner@heimrich-hannot.de>
2017-07-25 10:43:02 +0200

    added NewsTagsModel and NewsFeedGenerator class

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -4,3 +4,6 @@
  * Models
  */
 $GLOBALS['TL_MODELS']['tl_news'] = '\HeimrichHannot\NewsBundle\NewsModel';
+$GLOBALS['TL_MODELS']['tl_news_tags'] = '\HeimrichHannot\NewsBundle\NewsTagsModel';
+
+$GLOBALS['TL_HOOKS']['generateXmlFiles'][] = ['\HeimrichHannot\NewsBundle\NewsFeedGenerator','generateFeedsByTag'];
