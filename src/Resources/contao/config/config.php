9fb357599c84a5308bc5f6c24d344bc1b69310af
Konstantin Wagner <k.wagner@heimrich-hannot.de>
2017-07-26 09:53:59 +0200

    contact box

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -4,3 +4,17 @@
  * Models
  */
 $GLOBALS['TL_MODELS']['tl_news'] = '\HeimrichHannot\NewsBundle\NewsModel';
+
+
+/**
+ * Front end modules
+ */
+array_insert(
+    $GLOBALS['FE_MOD'],
+    2,
+    [
+        'news' => [
+            'news_contact_box' => 'Dav\NewsBundle\Module\ModuleNewsContactBox',
+        ],
+    ]
+);
