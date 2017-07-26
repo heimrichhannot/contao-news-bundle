f8c600b6fbb3feccaeb8e1c910e1b7d0fdeeed9b
koertho <t.koerner@heimrich-hannot.de>
2017-07-26 12:13:19 +0200

    add dynamic feed option

--- src/Resources/contao/dca/tl_news_feed.php
+++ src/Resources/contao/dca/tl_news_feed.php
@@ -13,15 +13,32 @@ $dc = &$GLOBALS['TL_DCA']['tl_news_feed'];
 
 $dc['palettes']['default'] = str_replace('archives', 'archives,sources', $dc['palettes']['default']);
 
+
+$dc['palettes']['default'] = str_replace('format', 'format,feedGeneration', $dc['palettes']['default']);
+
+
 $fields = [
-    'sources' => [
+    'sources'        => [
         'label'            => &$GLOBALS['TL_LANG']['tl_news_feed']['sources'],
         'exclude'          => true,
         'inputType'        => 'checkbox',
         'filter'           => true,
-        'options_callback' => ['app.news_feed_generator','getDcaSourceOptions'],
+        'options_callback' => ['app.news_feed_generator', 'getDcaSourceOptions'],
         'eval'             => ['multiple' => true],
         'sql'              => "blob NULL"
+    ],
+    'feedGeneration' => [
+        'label'     => &$GLOBALS['TL_LANG']['tl_news_feed']['feedGeneration'],
+        'default'   => 'xml',
+        'exclude'   => true,
+        'filter'    => true,
+        'inputType' => 'select',
+        'options'   => [
+            'xml'     => $GLOBALS['TL_LANG']['tl_news_feed']['feedGeneration_xml'],
+            'dynamic' => $GLOBALS['TL_LANG']['tl_news_feed']['feedGeneration_dynamic'],
+        ],
+        'eval'      => ['tl_class' => 'w50'],
+        'sql'       => "varchar(32) NOT NULL default ''"
     ]
 ];
 
--- src/Resources/contao/languages/en/tl_news_feed.php
+++ src/Resources/contao/languages/en/tl_news_feed.php
@@ -6,3 +6,15 @@ $arrLang = &$GLOBALS['TL_LANG']['tl_news_feed'];
  * Fields
  */
 $arrLang['sources']        = ['News sources', 'Select desired news sources.'];
+$arrLang['feedGeneration']        = ['Newsfeed output', 'Choose yor desired newsfeed output. A dynamic feed is generated every time the feed is called and can be customized by GET parameters.'];
+
+/**
+ * Feed sources
+ */
+$arrLang['source_tag'] = "Tags";
+
+/**
+ * Feed generation options
+ */
+$arrLang['feedGeneration_xml'] = "XML File";
+$arrLang['feedGeneration_dynamic'] = "Dynamic Feed";
