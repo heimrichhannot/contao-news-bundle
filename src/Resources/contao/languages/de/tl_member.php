c42c02716eeb7de17207d86cd0c0758567fcfeb6
Rico Kaltofen <r.kaltofen@heimrich-hannot.de>
2018-03-02 10:30:40 +0100

    2.2.9, check changelig

--- src/Resources/contao/languages/de/tl_member.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$arrLang = &$GLOBALS['TL_LANG']['tl_member'];
-
-/**
- * Fields
- */
-$arrLang['title']        = ['Titel', 'Bitte den Titel eingeben'];
-$arrLang['organization'] = ['Organisation', 'Zugehörigkeit zu einer Organisation.'];
